438ddbb586d0424fd2463a4e0a67e468c92aee7b
Chun Yin Ho Wesley <choo0014@student.monash.edu>
2019-09-23 12:25:07 +1000

    form made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  message = "0";
+  data = [];
+  bookTitle = '';
+  date = '';
+  bookType = '';
+  bookSummary = '';
+  numHardCover = 0;
+
+  newItem() {
+    this.data.push({
+      bookTitle: this.bookTitle,
+      date: this.date,
+      bookType: this.bookType,
+      bookSummary: this.bookSummary,
+    });
+    if (this.bookType === 'Hard Cover') {
+      this.numHardCover++;
+    }
+  }
+  deleteBook(x) {
+    this.data.splice(x, 1);
+  }
+  deleteHardCover() {
+    let x = this.data.length;
+
+  }
 }
