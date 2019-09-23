deb0265b445af7c26b73b93c412a91ee531577da
Chun Yin Ho Wesley <choo0014@student.monash.edu>
2019-09-23 16:17:50 +1000

    better count HardCover

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,27 +12,38 @@ export class AppComponent {
   date: string = '';
   bookType: string = '';
   bookSummary: string = '';
-  numHardCover: number = 0;
+  // numHardCover: number = 0;
 
-  newItem() {
+  newItem(): void{
     this.data.push({
       bookTitle: this.bookTitle,
       date: this.date,
       bookType: this.bookType,
       bookSummary: this.bookSummary
     });
-    if (this.bookType === 'Hard Cover') {
-      this.numHardCover++;
-    }
+    // if (this.bookType === 'Hard Cover') {
+    //   this.numHardCover++;
+    // }
   }
-  deleteBook(index: number) {
-    if (this.data[index].bookType === 'Hard Cover'){
-      this.numHardCover--;
-    }
+  deleteBook(index: number): void {
+    // if (this.data[index].bookType === 'Hard Cover'){
+    //   this.numHardCover--;
+    // }
     this.data.splice(index, 1);
   }
   deleteHardCover() {
     let x = this.data.length;
 
   }
+  getHardCoverNo(): number {
+    let numHardCover = 0;
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i].bookType === 'Hard Cover') {
+        numHardCover++;
+      }
+    }
+    return numHardCover;
+  }
+
 }
